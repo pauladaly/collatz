53a803616ffd3362658eaa79b2f04cf92ecf1012
Daly <paula.daly@dxc.com>
2019-09-25 17:55:25 +0100

    Added readme

--- collatz.py
+++ collatz.py
@@ -1,5 +1,5 @@
 # The number we will perform the Collatz operations on,
-n = 20
+n = int(input("Enter a positve ineger: "))
 
 # Keep looping until we reach 1.
 # Note: this assumes teh Collatz conjecture is true.
@@ -8,8 +8,8 @@ while n != 1:
   print(n)
   # Check if n is even.
   if n % 2 == 0:
-     If n is even, divide it by 2
-    n = n / 2
+    # If n is even, divide it by 2
+    n = n // 2
   else:
     # If n is odd, multiply by 3 and add 1.
     n = (3* n) + 1
